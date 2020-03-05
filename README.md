3eee4a89351876378b24018a80606e551e099f2b
Christie Nguyen <christie.nguyen91@gmail.com>
2020-03-05 23:05:32 +0000

    Automatically backed up by Learn

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Variable Assignment
+
 
 ## Objectives
 
--- variable.rb
+++ variable.rb
@@ -1,2 +1,15 @@
  # Define a local variable called 'greeting',
  # give it the value of the string "Hello World"
+
+
+puts 1+1 
+puts 1+1 
+
+Example = "the word 'Example' is equal to the sentence, it named variable."
+
+puts example 
+puts example
+puts example 
+
+puts "variables are any previously undefined word that"
+puts "starts with a lowercase letter."
